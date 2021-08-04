09d0fc1229e8c4ec2a74d2c07453fd56f03bd151
Clement Carre <clement.carre@bionomeex.com>
2021-08-04 17:56:34 +0200

    Add Help command

--- Remipedia/Modules/ML.cs
+++ Remipedia/Modules/ML.cs
@@ -10,6 +10,15 @@ namespace Remipedia.Modules
 {
     public class ML : ModuleBase
     {
+        [Command("Help")]
+        public async Task HelpAsync() {
+            await ReplyAsync(embed: new EmbedBuilder{
+                Color = new Color(0x067FBD),
+                Title = "Help",
+                Description = "**Dream [layer] [iters] [range]**:\nDream use VGG-16 as a backbone so only 18 layers are usable.\n\t[layer] optional (1 - 19)(default to 10) choose the layer you want to use for gradient maximization. Low level (near 0) will show basics changes (colors, straight lines, etc...), whereas high level (near 16) will show more animal structures.\n\t[iters] optional (1 - 100)(default to 10). Number of iterations to apply to the image, the more iterations, th more feverish the DREAM will be!\n\t[range] optional (1 - 19-layer)(default to 1). Number of layers after the selected layer (with layer argument) that will be choosen at random for each iteration. Large number will makes the DREAM more diverse in the range of possibles transformations."
+            }.Build());
+        }
+
         [Command("Dream", RunMode = RunMode.Async)]
         public async Task DreamAsync(int layer = 10, int iters = 10, int range = 1)
         {
