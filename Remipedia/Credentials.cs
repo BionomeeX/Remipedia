3f2a3de11233b4aab372a95a78497d9896089fb0
Christian Chaux <christian.chaux@bionomeex.com>
2021-08-04 16:50:13 +0200

    Add empty Discord bot

---
+++ Remipedia/Credentials.cs
@@ -0,0 +1,7 @@
+﻿namespace Remipedia
+{
+    public class Credentials
+    {
+        public string BotToken { init; get; }
+    }
+}
---
+++ Remipedia/Modules/ML.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Commands;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Remipedia.Modules
+{
+    public class ML : ModuleBase
+    {
+        [Command("Dream")]
+        public async Task DreamAsync()
+        {
+            if (Context.Message.Attachments.Count == 0)
+            {
+                await ReplyAsync("You need to provide an image");
+                return;
+            }
+            var fi = new FileInfo(Context.Message.Attachments.ElementAt(0).Url);
+            if (fi.Extension != ".png" && fi.Extension != ".jpg" && fi.Extension != ".jpeg")
+            {
+                await ReplyAsync("Invalid file type");
+                return;
+            }
+        }
+    }
+}
