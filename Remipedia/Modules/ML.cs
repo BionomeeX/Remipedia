37a836f226d13d917e42c4cba10b96ff21f2ac42
Xwilarg <xwilarg@yahoo.fr>
2021-08-05 10:31:51 +0200

    Improve help

--- Remipedia/Modules/ML.cs
+++ Remipedia/Modules/ML.cs
@@ -15,7 +15,26 @@ namespace Remipedia.Modules
             await ReplyAsync(embed: new EmbedBuilder{
                 Color = new Color(0x067FBD),
                 Title = "Help",
-                Description = "**Dream [layer] [iters] [range]**:\nDream use VGG-16 as a backbone so only 18 layers are usable.\n\t[layer] optional (1 - 19)(default to 10) choose the layer you want to use for gradient maximization. Low level (near 0) will show basics changes (colors, straight lines, etc...), whereas high level (near 16) will show more animal structures.\n\t[iters] optional (1 - 100)(default to 10). Number of iterations to apply to the image, the more iterations, th more feverish the DREAM will be!\n\t[range] optional (1 - 19-layer)(default to 1). Number of layers after the selected layer (with layer argument) that will be choosen at random for each iteration. Large number will makes the DREAM more diverse in the range of possibles transformations."
+                Fields = new()
+                {
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Dream [layer] [iters] [range]",
+                        Value = @"Dream use VGG-16 as a backbone so only 18 layers are usable.\n
+[layer] optional (1 - 19)(default to 10) choose the layer you want to use for gradient maximization.
+Low level (near 0) will show basics changes (colors, straight lines, etc...), whereas high level (near 16) will show more animal structures.\n
+
+[iters] optional (1 - 100)(default to 10). Number of iterations to apply to the image, the more iterations, the more feverish the DREAM will be!\n
+
+[range] optional (1 - 19-layer)(default to 1). Number of layers after the selected layer (with layer argument) that will be choosen at random for each iteration.
+Large number will makes the DREAM more diverse in the range of possibles transformations."
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Edge [percentile]",
+                        Value = "todo"
+                    }
+                }
             }.Build());
         }
 
