0e2092006e3caf5bb4449627540051c5d97d4b41
Xwilarg <xwilarg@yahoo.fr>
2021-08-07 18:30:14 +0200

    Add user agent to HttpClient

--- Remipedia/StaticObjects.cs
+++ Remipedia/StaticObjects.cs
@@ -7,5 +7,10 @@ namespace Remipedia
     {
         public static HttpClient HttpClient { get; } = new();
         public static Random Random { get; } = new();
+
+        static StaticObjects()
+        {
+            HttpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 Remipedia");
+        }
     }
 }
